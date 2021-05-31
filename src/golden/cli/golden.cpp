df02f4e1f7f866edf14d04975ef584130b908a9a
nokx <72279573+nokx5@users.noreply.github.com>
2021-05-31 13:20:21 +0200

    better organization

---
+++ src/golden/cli/golden.cpp
@@ -0,0 +1,7 @@
+#include "golden/include/gold.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "Welcome to nokx cpp golden project!" << std::endl;
+  return 0;
+}
---
+++ src/golden/include/gold.hpp
@@ -0,0 +1,8 @@
+
+#pragma once
+
+namespace golden {
+
+inline int add(int a, int b) { return a + b; }
+
+} // namespace golden
