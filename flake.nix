f425fb98582b4360eb71ac6b19a7165d2d4e8e04
nokx <72279573+nokx5@users.noreply.github.com>
2021-07-13 17:05:21 +0200

    remove numtides from flake

--- flake.nix
+++ flake.nix
@@ -1,62 +1,104 @@
 {
-  description = "A simple C++/Python Binding flake";
+  description = "A simple pybind11 flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        overlay = pkgs-self: pkgs-super:
+  outputs = { self, nixpkgs }:
+    let
+      forCustomSystems = custom: f: nixpkgs.lib.genAttrs custom (system: f system);
+      allSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" ];
+      devSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = forCustomSystems allSystems;
+      forDevSystems = forCustomSystems devSystems;
+
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ self.overlay ];
+        }
+      );
+
+      repoName = "golden-pybind11";
+      repoVersion = nixpkgsFor.x86_64-linux.python3Packages.golden-pybind11.version;
+      repoDescription = "golden-pybind11 - A simple pybind11 flake";
+    in
+    {
+    overlay = final: pred:
           let
-            inherit (pkgs-super.lib) composeExtensions;
+            inherit (pred.lib) composeExtensions;
             pythonPackageOverrides = python-self: python-super: {
               golden-pybind11 = python-self.callPackage ./derivation.nix {
                 src = self;
-                stdenv = pkgs-self.gccStdenv;
+                stdenv = final.gccStdenv;
               };
               golden-pybind11-clang = python-self.callPackage ./derivation.nix {
                 src = self;
-                stdenv = pkgs-self.clangStdenv;
+                stdenv = final.clangStdenv;
               };
               # project_setuptools =
               #   python-self.callPackage ./derivation-setuptools.nix {
               #     src = self;
               #   };
-              fullDev = python-self.callPackage ./shell.nix { };
             };
           in {
-            python37 = pkgs-super.python37.override (old: {
+            python37 = pred.python37.override (old: {
               packageOverrides =
                 composeExtensions (old.packageOverrides or (_: _: { }))
                 pythonPackageOverrides;
             });
-            python38 = pkgs-super.python38.override (old: {
+            python38 = pred.python38.override (old: {
               packageOverrides =
                 composeExtensions (old.packageOverrides or (_: _: { }))
                 pythonPackageOverrides;
             });
-            python39 = pkgs-super.python39.override (old: {
+            python39 = pred.python39.override (old: {
               packageOverrides =
                 composeExtensions (old.packageOverrides or (_: _: { }))
                 pythonPackageOverrides;
             });
-            python3 = pkgs-self.python38;
+            python3 = final.python38;
           };
 
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [ overlay ];
+      devShell = forDevSystems (system:
+        let pkgs = nixpkgsFor.${system}; in pkgs.callPackage ./shell.nix { clangSupport = false; }
+      );
+
+      hydraJobs = {
+        build = forDevSystems (system: nixpkgsFor.${system}.python3Packages.golden-pybind11);
+        build-clang = forDevSystems (system: nixpkgsFor.${system}.python3Packages.golden-pybind11-clang);
+      };
+      packages = forAllSystems (system:
+        with nixpkgsFor.${system}; {
+          inherit golden-pybind11 golden-pybind11-clang;
+        });
+
+      defaultPackage = forAllSystems (system:
+        self.packages.${system}.golden-pybind11);
+
+      apps = forAllSystems (system: {
+        golden-pybind11 = {
+          type = "app";
+          program = "${self.packages.${system}.golden-pybind11}/bin/cli_golden";
         };
-      in {
-        packages = {
-          inherit (pkgs.python3Packages)
-            fullDev golden-pybind11 golden-pybind11-clang;
+        golden-pybind11-clang = {
+          type = "app";
+          program = "${self.packages.${system}.golden-pybind11-clang}/bin/cli_golden";
         };
-        defaultPackage = self.packages.${system}.golden-pybind11;
-        devShell = self.packages.${system}.golden-pybind11;
-      });
+      }
+      );
+
+      defaultApp = forAllSystems (system: self.apps.${system}.golden-pybind11);
+
+      templates = {
+        golden-pybind11 = {
+          description = "template - ${repoDescription}";
+          path = ./.;
+        };
+      };
+
+      defaultTemplate = self.templates.golden-pybind11;
+    };
 }
