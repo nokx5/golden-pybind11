25de66d9c7deab49a93f5f4a9fbe1ab3f724202d
nokx <72279573+nokx5@users.noreply.github.com>
2021-08-19 10:48:10 +1000

    correct typo

--- src/golden/cli/golden.cpp
+++ src/golden/cli/golden.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Welcome to nokx cpp golden project!" << std::endl;
+  std::cout << "Welcome to nokx golden pybind11 project!" << std::endl;
   return 0;
 }
